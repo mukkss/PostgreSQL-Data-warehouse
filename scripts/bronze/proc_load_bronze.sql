a1b194aa3ffa0ab2002fe610c260a8a9f3639953
mukkss <muksss1102@gmail.com>
2025-09-20 00:40:12 +0530

    Add procedure to load data into bronze layer with error handling

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,88 @@
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    batch_start_time TIMESTAMP;
+    batch_end_time TIMESTAMP;
+BEGIN
+    batch_start_time := NOW();
+    RAISE NOTICE '================================================';
+    RAISE NOTICE 'Loading Bronze Layer';
+    RAISE NOTICE '================================================';
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading CRM Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+    -- CRM_CUST_INFO
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
+    TRUNCATE TABLE bronze.crm_cust_info;
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
+    COPY bronze.crm_cust_info FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_crm\cust_info.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    -- CRM_PRD_INFO
+    start_time := NOW();
+    TRUNCATE TABLE bronze.crm_prd_info;
+    COPY bronze.crm_prd_info FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_crm\prd_info.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    -- CRM_SALES_DETAILS
+    start_time := NOW();
+    TRUNCATE TABLE bronze.crm_sales_details;
+    COPY bronze.crm_sales_details FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_crm\sales_details.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading ERP Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+    -- ERP_LOC_A101
+    start_time := NOW();
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    COPY bronze.erp_loc_a101 FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_erp\LOC_A101.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    -- ERP_CUST_AZ12
+    start_time := NOW();
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    COPY bronze.erp_cust_az12 FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_erp\CUST_AZ12.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    -- ERP_PX_CAT_G1V2
+    start_time := NOW();
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    COPY bronze.erp_px_cat_g1v2 FROM 'D:\Projects\PostgreSQL-Data-warehouse\datasets\source_erp\PX_CAT_G1V2.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM (end_time - start_time));
+
+    batch_end_time := NOW();
+    RAISE NOTICE '==========================================';
+    RAISE NOTICE 'Loading Bronze Layer is Completed';
+    RAISE NOTICE '   - Total Load Duration: % seconds', EXTRACT(EPOCH FROM (batch_end_time - batch_start_time));
+    RAISE NOTICE '==========================================';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE '==========================================';
+        RAISE NOTICE 'ERROR OCCURRED DURING LOADING BRONZE LAYER';
+        RAISE NOTICE 'Error Message: %', SQLERRM;
+        RAISE NOTICE '==========================================';
+END;
+$$;
+
+CALL bronze.load_bronze();
