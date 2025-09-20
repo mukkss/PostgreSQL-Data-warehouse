fa81eeb1116f707f4c15a9f647aaab64522e6fe8
mukkss <muksss1102@gmail.com>
2025-09-20 20:31:40 +0530

    Update DDL script for silver tables: modify data types for product and sales details, and add missing category ID field

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -30,12 +30,13 @@ DROP TABLE IF EXISTS silver.crm_prd_info CASCADE;
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       VARCHAR(50),
     prd_key      VARCHAR(50),
     prd_nm       VARCHAR(50),
     prd_cost     INT,
     prd_line     VARCHAR(50),
-    prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -46,9 +47,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num  VARCHAR(50),
     sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT,
